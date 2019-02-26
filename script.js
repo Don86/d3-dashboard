ef5fa106c369c4aeb81053ab4bfda0c1c38bfcd9
Don86 <dh.teng.86@gmail.com>
2019-02-26 18:57:15 +1100

    added dummy bar chart and dummy input field

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function parseData(myInput) {
   
     return newArray
   }
+
+function computeBarData(initialValue) {
+    let barArray = [initialValue]
+    const contrib = 1000
+    const numYears = 20
+    const ir = 0.0287
+    for (var i = 0; i < numYears ; i++) {
+        let oldVal = barArray[barArray.length-1]
+        console.log(oldVal)
+        let newVal = (oldVal+contrib)*(1+ir)
+        barArray.push(newVal)
+    }
+    return barArray
+}
